fb2a94b5a1def682c25bf3c3403e15739661196e
Prachi <prachi4856@gmail.com>
2025-12-22 14:26:45 +0530

    Day 1: added frontend placeholder UI, cleaned default React files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>ClassSnap Attendance System</h1>
+        <p> Teacher Dashboard</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
